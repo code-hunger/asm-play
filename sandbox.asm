b8f67c126aadacc426e0ace18c4acb616c49708c
Code Hunger <beinghavingbreackfast@gmail.com>
2019-07-27 22:31:34 +0300

    Added printing to show the result of the program

--- sandbox.asm
+++ sandbox.asm
@@ -7,18 +7,31 @@ section .text
 global _start
 _start:
             nop
-            mov rax,MyStr
+            mov rax, MyStr  ; The text is in RAX
+            mov rdx, rax    ; Keep a copy of the message start in RDX
+            mov rbx, 0      ; The length is in RBX
 
-ProcChar:   mov cl,[rax]        ; Look at the first byte of our string
-            cmp cl,0            ; Check if our byte is zero:
-            je Exit             ;       in that case, finish.
-            add byte [rax],32   ; Convert an ASCII letter to uppercase
+ProcChar:   mov cl, [rax]       ; Look at the first byte of our string
+            cmp cl, 0           ; Check if our byte is zero:
+            je Print            ;       in that case, finish.
+
+            add byte [rax], 32d ; Otherwise, convert an ASCII letter to uppercase
             inc rax             ; Move the string pointer to the next char
+            inc rbx             ; Increment the string length
             jmp ProcChar        ; Process the next char
 
-Exit:
-            mov rax,1   ; Prepare to call sys_exit
-            mov rbx,0   ; with exit code 0
+Print:      mov byte [rax], 10  ; Add a newline to the end
+            inc rbx             ; And update the length respectfully
+
+            ; Now RAX points to the end of the string. The start is still in RDX.
+            mov rcx, rdx    ; Move our message from RDX to RCX 
+            mov rdx, rbx    ; Move the message length to RDX
+            mov eax, 4      ; Specify a sys_write call
+            mov ebx, 1      ; ... to stdout
+            int 80h         ; Fire!
+
+Exit:       mov rax,1   ; Prepare to call sys_exit
+            mov rbx,0   ; ... with exit code 0
             int 80h     ; Fire!
 
 section .bss
