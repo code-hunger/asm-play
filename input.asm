ec748276c9cbc013594ff9d6572473ff85619d1f
Code Hunger <beinghavingbreackfast@gmail.com>
2019-07-28 15:21:00 +0300

    Fixed broken order of execution & improved comments

--- input.asm
+++ input.asm
@@ -1,8 +1,11 @@
 section .data
 
-section .bss
+section .rodata
 
     BUFFER_LENGTH equ 100
+
+section .bss
+
     Buffer: resb BUFFER_LENGTH
 
 section .text
@@ -11,42 +14,41 @@ global _start
 _start:
     nop
 
-        mov rsi, 0              ; Keep the full msg length in RSI
+Read:       mov rax, 3              ; Will do sys_read!
+            mov rbx, 0              ; From STDIN!
+            mov rcx, Buffer         ; Into our buffer
+            mov rdx, BUFFER_LENGTH  ; With this length
 
-Read:   mov rax, 3              ; Will do sys_read!
-        mov rbx, 0              ; From STDIN!
-        mov rcx, Buffer         ; Into our buffer
-        mov rdx, BUFFER_LENGTH  ; With this length
+            int 80h                 ; Fire!
 
-        int 80h                 ; Fire!
+            cmp rax, 0              ; Check if at EOF?
+            je Exit                 ; If no more input, exit!
+            mov rdx, rax            ; Keep a copy of the length of the buffer in RDX
 
-        add rsi, rax    ; Keep the full message length in ESI
-        cmp rax, 0      ; Check if at EOF?
-        je Print        ; If no more input, exit!
+CheckRange: cmp byte [rcx], 'a'     ; If byte is out of range, no need for uppercasing.
+            jl SwitchNext           ; Equivalent to:
+            cmp byte [rcx], 'z'     ; if rcx < 'a' or rcx > 'z'
+            jg SwitchNext           ;     SwitchNext            
 
-        mov rbx, rax    ; Set the current buffer's length in RBX
-        mov rax, Buffer ; Use RAX as a pointer to the buffer
+ModifyByte: sub byte [rcx], 32      ; Convert to uppercase
 
-Process:    ; Check if current byte is within a..z:
-            cmp byte [rax], 'a' 
-            jl Next
-            cmp byte [rax], 'z'
-            jg Next
+SwitchNext: inc rcx                 ; Move pointer to the next byte
+            dec rax                 ; Check if we reached the end:
+            jnz CheckRange          ;       if so - repeat.
 
-            inc byte [rax]  ; Modify current byte
+Print:      mov rax, 4              ; Prepare a sys_write call
+            mov rbx, 1              ;                 ...to STDOUT
 
-Next:       inc rax         ; Move to the next byte
-            dec rbx         ; 
-            jnz Process
-            jmp Read
+            ; Note that the following can also be done with 'sub rcx, rdx' 
+            ; because currently RCX points right after the end of the buffer.
+            mov rcx, Buffer         ; The message being our buffer,
+                                    ;   ...it's length is already in RDX
 
-Print:      ;mov rcx, rax    ; Move the message to RCX
-            mov rdx, rsi    ; Move the length to RDX
-            mov rax, 4      ; Prepare a sys_write
-            mov rbx, 1      ;   ...to STDOUT
-            int 80h         ; Fire!
+            int 80h                 ; Fire!
 
-Exit:       mov rax,1       ; Prepare to call sys_exit
-            mov rbx,0       ; ... with exit code 0
-            int 80h         ; Fire!
+            jmp Read                ; The old buffer was printed. Read a new one!
+
+Exit:       mov rax,1               ; Prepare to call sys_exit
+            mov rbx,0               ; ... with exit code 0
+            int 80h                 ; Fire!
 
